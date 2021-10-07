f9d9ee0ef3d27eff3bac238a22d9112856f7d585
Ian Denhardt <ian@zenhack.net>
2021-10-06 21:56:49 -0400

    Set up git correctly in vagrant.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -5,6 +5,12 @@ cd /opt/app
 
 [ -d hedgedoc ] || {
 	./scripts/checkout
+	(
+		cd hedgedoc
+		# Set these, or git-am will complain:
+		git config user.email vagrant-spk@localhost
+		git config user.name 'Vagrant SPK User'
+	)
 	./scripts/apply-patches
 }
 cd hedgedoc
